d9d57365d10fb7e98c8767a63948dd89ad10110d
Albert <101507110+kkk7k@users.noreply.github.com>
2022-03-21 15:40:58 +0300

    /

---
+++ ConsoleApp_Events/ConsoleApp_Events/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Key key = new Key();
+key.OnKeyPressed += PrintChar;
+key.Run();
+
+void PrintChar(object o, char ch)
+{
+    Console.WriteLine("\nEntered character: "+ch+"\n");
+}
+    
+class Key
+{
+    public event EventHandler<char>? OnKeyPressed;
+    public void Run()
+    {
+        while (true)
+        {
+            Console.Write("Enter a character: ");
+            char sim = Console.ReadKey().KeyChar;
+            if (sim != 'c')
+            {
+                OnKeyPressed?.Invoke(this, sim);
+            }
+            else
+            {
+                Console.WriteLine("\nShut down");
+                break;
+            }
+        }
+    }
+}

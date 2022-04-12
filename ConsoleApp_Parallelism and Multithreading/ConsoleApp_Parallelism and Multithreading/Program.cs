e53442527ac4318f27a734a5da4f4e9c0f99c963
Albert <101507110+kkk7k@users.noreply.github.com>
2022-04-12 07:35:01 +0300

    Task Parallelism and Multithreading Complete

---
+++ ConsoleApp_Parallelism and Multithreading/ConsoleApp_Parallelism and Multithreading/Program.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp_Parallelism_and_Multithreading;
+
+
+
+Console.WriteLine("Приложение запущено");
+string? mes = null;
+
+while (mes != "/exit")
+{
+    Console.WriteLine("Введите текст запроса для отправки. Для выхода введите /exit");
+    mes = Console.ReadLine();
+    if (mes=="/exit")
+        break;
+    Console.WriteLine($"Будет послано сообщение '{mes}'");
+    ContainMessage();
+    var thread = new Thread(MethodRequest);
+    thread.Start();
+    
+}
+Console.WriteLine("Приложение завершает работу.");
+
+
+void ContainMessage()
+{
+    string[] args = new string[20];
+    Console.WriteLine("Введите аргументы сообщения. Если аргумент не нужны - введите /end");
+    string newArg = Console.ReadLine();
+    int i = 0;
+    while (newArg != "/end")
+    {
+        args[i] = newArg;
+        Console.WriteLine("Введите следующий аргумент сообщения. Для окончания добавления аргуметнов введите /end");
+        newArg = Console.ReadLine();
+        i++;
+    }
+}
+
+void MethodRequest()
+{
+    var gui = Guid.NewGuid();
+    try
+    {
+        Console.WriteLine($"Было отправлено сообщение '{mes}'. Присвоен идентификатор {gui}");
+        DummyRequestHandler request = new DummyRequestHandler();
+        string res = request.HandleRequest(mes, args);
+        Console.WriteLine($"Сообщение с идентификатором '{gui}'получило ответ - {res}");
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"Сообщение с идентификатором '{gui}' упало с ошибкой:{ex.Message}");
+    }
+}
+

7c043225894474f8bec036d3597a6f867b1875c2
Albert <101507110+kkk7k@users.noreply.github.com>
2022-04-13 09:47:39 +0300

    Correction

--- ConsoleApp_Parallelism and Multithreading/ConsoleApp_Parallelism and Multithreading/Program.cs
+++ ConsoleApp_Parallelism and Multithreading/ConsoleApp_Parallelism and Multithreading/Program.cs
@@ -9,45 +9,43 @@ while (mes != "/exit")
 {
     Console.WriteLine("Введите текст запроса для отправки. Для выхода введите /exit");
     mes = Console.ReadLine();
-    if (mes=="/exit")
+    if (mes == "/exit")
         break;
     Console.WriteLine($"Будет послано сообщение '{mes}'");
-    ContainMessage();
-    var thread = new Thread(MethodRequest);
-    thread.Start();
     
-}
-Console.WriteLine("Приложение завершает работу.");
-
-
-void ContainMessage()
-{
-    string[] args = new string[20];
+    
+    var argusList = new List<string>();
     Console.WriteLine("Введите аргументы сообщения. Если аргумент не нужны - введите /end");
     string newArg = Console.ReadLine();
-    int i = 0;
+
     while (newArg != "/end")
     {
-        args[i] = newArg;
+        argusList.Add(newArg);
         Console.WriteLine("Введите следующий аргумент сообщения. Для окончания добавления аргуметнов введите /end");
         newArg = Console.ReadLine();
-        i++;
-    }
-}
 
-void MethodRequest()
-{
-    var gui = Guid.NewGuid();
-    try
-    {
-        Console.WriteLine($"Было отправлено сообщение '{mes}'. Присвоен идентификатор {gui}");
-        DummyRequestHandler request = new DummyRequestHandler();
-        string res = request.HandleRequest(mes, args);
-        Console.WriteLine($"Сообщение с идентификатором '{gui}'получило ответ - {res}");
     }
-    catch(Exception ex)
+    string[] argusArray = argusList.ToArray();
+    
+
+    var thread = new Thread(MethodRequest);
+    thread.Start();
+
+    void MethodRequest()
     {
-        Console.WriteLine($"Сообщение с идентификатором '{gui}' упало с ошибкой:{ex.Message}");
+        var gui = Guid.NewGuid();
+        try
+        {
+            Console.WriteLine($"Было отправлено сообщение '{mes}'. Присвоен идентификатор {gui}");
+            DummyRequestHandler request = new DummyRequestHandler();
+            string res = request.HandleRequest(mes, argusArray);
+            Console.WriteLine($"Сообщение с идентификатором '{gui}'получило ответ - {res}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Сообщение с идентификатором '{gui}' упало с ошибкой:{ex.Message}");
+        }
     }
 }
+Console.WriteLine("Приложение завершает работу.");
 
